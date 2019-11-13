93bf758cf5c2e580f641b2f094ca30e6a4a4d4c7
kevin schuchard <schuchard.kevin@gmail.com>
2019-11-12 20:47:28 -0700

    fix linting issues for `ng lint`

--- sandbox/src/app/app.component.spec.ts
+++ sandbox/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed, async } from "@angular/core/testing";
-import { AppComponent } from "./app.component";
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent]
     }).compileComponents();
   }));
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -17,15 +17,15 @@ describe("AppComponent", () => {
   it(`should have as title 'sandbox'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("sandbox");
+    expect(app.title).toEqual('sandbox');
   });
 
-  it("should render title in a h1 tag", () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to sandbox!"
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to sandbox!'
     );
   });
 });
