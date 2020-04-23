f5896ce35d7ea39af5144dc385dd720616c6df55
Taylor Buckner <taylora.buckner@gmail.com>
2020-04-23 07:59:55 -0500

    fix(typescript): adjust how custom commands are suggested

--- src/schematics/cypress/files/cypress/support/commands.ts
+++ src/schematics/cypress/files/cypress/support/commands.ts
@@ -1,4 +1,22 @@
 // ***********************************************
+// This example namespace declaration will help
+// with Intellisense and code completion in your
+// IDE or Text Editor.
+// ***********************************************
+// declare namespace Cypress {
+//   interface Chainable<Subject = any> {
+//     customCommand(param: any): typeof customCommand;
+//   }
+// }
+//
+// function customCommand(param: any): void {
+//   console.warn(param);
+// }
+//
+// NOTE: You can use it like so:
+// Cypress.Commands.add('customCommand', customCommand);
+// 
+// ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
