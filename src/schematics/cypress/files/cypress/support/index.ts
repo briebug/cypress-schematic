ac69b943caff9bda38c004dfaf2ceddc9186d293
Taylor Buckner <taylora.buckner@gmail.com>
2020-04-23 07:59:55 -0500

    fix(typescript): adjust how custom commands are suggested

--- src/schematics/cypress/files/cypress/support/index.ts
+++ src/schematics/cypress/files/cypress/support/index.ts
@@ -13,5 +13,5 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// When a command from ./commands is ready to use, import with `import * as commands from './commands'` syntax
-// import * as commands from './commands';
+// When a command from ./commands is ready to use, import with `import './commands'` syntax
+// import './commands';
