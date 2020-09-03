3b18554c70955c3f61c1fe6387bc7a7428fecad7
Marvin Heilemann <marvin.heilemann@googlemail.com>
2020-09-03 18:53:02 +0200

    adjusted number of scaffolded files in test because of updated schematics

--- src/schematics/cypress/index_spec.ts
+++ src/schematics/cypress/index_spec.ts
@@ -1,6 +1,6 @@
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 
-const NUMBER_OF_SCAFFOLDED_FILES = 38;
+const NUMBER_OF_SCAFFOLDED_FILES = 37;
 
 describe('@briebug/cypress-schematic', async () => {
   it('works', async () => {
@@ -33,6 +33,10 @@ describe('@briebug/cypress-schematic', async () => {
       require.resolve('../collection.json')
     );
     const tree = await runner.runSchematicAsync('ng-add', {}, await getWorkspaceTree()).toPromise();
+    console.log(tree.files);
+    console.log(tree.readContent('/package.json'));
+    console.log(tree.readContent('/tsconfig.json'));
+    console.log(tree.readContent('/tsconfig.base.json'));
     expect(tree.files.length).toEqual(NUMBER_OF_SCAFFOLDED_FILES);
   });
 });
