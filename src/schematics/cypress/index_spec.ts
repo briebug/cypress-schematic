aadc51e2b7a6210a11aa3f36d8988dab9d0cfc6e
Marvin Heilemann <marvin.heilemann@googlemail.com>
2020-09-03 09:42:14 +0200

    increase min scaffold size to 38 because of solution style tsconfig

--- src/schematics/cypress/index_spec.ts
+++ src/schematics/cypress/index_spec.ts
@@ -1,17 +1,17 @@
-import { SchematicTestRunner } from "@angular-devkit/schematics/testing";
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 
-const NUMBER_OF_SCAFFOLDED_FILES = 37;
+const NUMBER_OF_SCAFFOLDED_FILES = 38;
 
-describe("@briebug/cypress-schematic", async () => {
-  it("works", async () => {
-    async function getWorkspaceTree(appName = "bar") {
-      const ngRunner = new SchematicTestRunner("@schematics/angular", "");
+describe('@briebug/cypress-schematic', async () => {
+  it('works', async () => {
+    async function getWorkspaceTree(appName = 'bar') {
+      const ngRunner = new SchematicTestRunner('@schematics/angular', '');
 
       const workspaceOptions = {
-        name: "workspace",
-        newProjectRoot: "projects",
-        version: "6.0.0",
-        defaultProject: appName
+        name: 'workspace',
+        newProjectRoot: 'projects',
+        version: '6.0.0',
+        defaultProject: appName,
       };
 
       const appOptions = {
@@ -19,26 +19,20 @@ describe("@briebug/cypress-schematic", async () => {
         inlineTemplate: false,
         routing: false,
         skipTests: false,
-        skipPackageJson: false
+        skipPackageJson: false,
       };
 
-      let appTree = await ngRunner
-        .runSchematicAsync("workspace", workspaceOptions)
-        .toPromise();
-      appTree = await ngRunner
-        .runSchematicAsync("application", appOptions, appTree)
-        .toPromise();
+      let appTree = await ngRunner.runSchematicAsync('workspace', workspaceOptions).toPromise();
+      appTree = await ngRunner.runSchematicAsync('application', appOptions, appTree).toPromise();
 
       return appTree;
     }
 
     const runner = new SchematicTestRunner(
-      "@briebug/cypress-schematic",
-      require.resolve("../collection.json")
+      '@briebug/cypress-schematic',
+      require.resolve('../collection.json')
     );
-    const tree = await runner
-      .runSchematicAsync("ng-add", {}, await getWorkspaceTree())
-      .toPromise();
+    const tree = await runner.runSchematicAsync('ng-add', {}, await getWorkspaceTree()).toPromise();
     expect(tree.files.length).toEqual(NUMBER_OF_SCAFFOLDED_FILES);
   });
 });
