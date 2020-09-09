f97cc68f4142c39528fb3af9c27b37e42fcf93da
Marvin Heilemann <marvin.heilemann@googlemail.com>
2020-09-09 13:21:39 +0200

    updating sandbox workspace

--- sandbox-workspace/projects/new-sandbox/src/app/app.component.spec.ts
+++ sandbox-workspace/projects/new-sandbox/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed, async } from "@angular/core/testing";
-import { AppComponent } from "./app.component";
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -17,15 +17,15 @@ describe("AppComponent", () => {
   it(`should have as title 'new-sandbox'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("new-sandbox");
+    expect(app.title).toEqual('new-sandbox');
   });
 
-  it("should render title", () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector(".content span").textContent).toContain(
-      "new-sandbox app is running!"
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'new-sandbox app is running!'
     );
   });
 });
